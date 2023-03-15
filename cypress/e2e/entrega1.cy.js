761d8400aae11a07e8e95dd2805d88f13d14808f
Daniel Farias <dj.luis198@gmail.com>
2023-03-15 12:48:44 -0300

    before-fixture-xpath

--- cypress/e2e/entrega1.cy.js
+++ cypress/e2e/entrega1.cy.js
@@ -1,5 +1,30 @@
+/// <reference types="cypress"/>
+
 describe('empty spec', () => {
+let loginData, productData;
+
+  before('before',()=>{
+
+    cy.fixture('login').then(dataLogin=>{
+      loginData=dataLogin;
+    })
+
+    cy.fixture('product').then(dataProduct=>{
+      productData=dataProduct;
+    })
+  })
+
+  beforeEach("beforeEach",()=>{
+    cy.visit("/");
+    cy.xpath('//span[@id="registertoggle"]').dblclick();
+    cy.xpath('//input[@id="user"]').type(loginData.user);
+    cy.xpath('//input[@id="pass"]').type(loginData.password);
+    cy.xpath('//button[@id="submitForm"]').click();
+    cy.xpath('//h2[contains(text(),loginData.user)]').should('exist');
+    cy.xpath('//a[@id="onlineshoplink"]').click();
+})
+
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  cy.log("paso");
   })
 })
